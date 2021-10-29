057109fdf176d2208576c842dbe0b3541778b33b
animacpac <56101332+animacpac@users.noreply.github.com>
2021-10-28 19:33:08 -0600

    Registration first look

---
+++ register.py
@@ -0,0 +1,9 @@
+from tkinter import *
+def main_screen():
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Notes 1.0")
+    Label (text = "Notes 1.0", bg = "grey", font = ("Calibri", 13)).pack()
+    Label (text = "").pack()
+    Button(text = "Login").pack()
+    Button(text = "Register").pack()
