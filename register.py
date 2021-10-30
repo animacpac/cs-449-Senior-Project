999ee04eadf6e5b8d1a3f032cad1b4c870889097
animacpac <56101332+animacpac@users.noreply.github.com>
2021-10-29 21:09:12 -0600

    Finished the login page.

--- register.py
+++ register.py
@@ -1,9 +1,43 @@
 from tkinter import *
+import os
+
+def delete2():
+    screen3.destroy()
+def delete3():
+    screen4.destroy()
+def delete4():
+    screen5.destroy()
+
+
+def login_sucess():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("Success")
+    screen3.geometry("150x100")
+    Label(screen3, text = "Login Sucess").pack()
+    Button(screen3, text="OK", command = delete2).pack()
+
+def password_not_recognized():
+    global screen4
+    screen4 = Toplevel(screen)
+    screen4.title("Success")
+    screen4.geometry("150x100")
+    Label(screen4, text = "Password not found").pack()
+    Button(screen4, text="OK", command = delete3).pack()
+
+def user_not_found():
+    global screen5
+    screen5= Toplevel(screen)
+    screen5.title("Success")
+    screen5.geometry("150x100")
+    Label(screen5, text = "Username not found").pack()
+    Button(screen5, text="OK", command = delete4).pack()
+
 def register_user():
     username_info = username.get()
     password_info = password.get()
 
-    file=open(username_info+".txt", "w")
+    file=open(username_info, "w")
     file.write (username_info+"\n")
     file.write (password_info)
     file.close()
@@ -12,6 +46,25 @@ def register_user():
     password_entry.delete(0, END)
 
     Label(screen1, text="Registration Successiful", fg = "green",font = ("Calibri", 13)).pack()
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = username_verify.get()
+
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+        else:
+            password_not_recognized()
+    else:
+        user_not_found()
+
 
 def register():
     global screen1
@@ -22,6 +75,7 @@ def register():
     global password
     global username_entry
     global password_entry
+    
     username = StringVar()
     password = StringVar()
 
@@ -40,6 +94,33 @@ def register():
 
   
 def login():
+    global screen2
+    global username_entry1
+    global password_entry1
+
+
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("300x250")
+    Label (screen2, text = "Please enter details below to login").pack()
+    Label (screen2, text = "").pack()
+    global username_verify
+    global password_verify
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    Label (screen2, text = "Username * ").pack()
+    username_entry1 = Entry (screen2, textvariable = username_verify)
+    username_entry1.pack()
+    Label (screen2, text = "").pack()
+    Label (screen2, text = "Password * ").pack()
+    password_entry1 = Entry (screen2, textvariable = password_verify)
+    password_entry1.pack()
+    Label (screen2, text = "").pack()
+
+    Button(screen2, text = "Login", height="1", width="10", command= login_verify).pack()
+
+
     print("Login session started")
 
 def main_screen():
